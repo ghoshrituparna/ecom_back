e3b4632a71633e81acc9f670d9cc25037bb71260
Rituparna Ghosh <rituparna.ghosh@hih7.com>
2023-05-12 11:06:11 +0530

    change in package.json

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,4 +1,4 @@
-const catchAsyncError = require("../middleware/catchAsyncError");
+const catchAsyncError = require("../middleware/catchasyncError");
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
