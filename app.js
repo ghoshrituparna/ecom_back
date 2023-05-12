6d00132690e9810814e434337e41de37cae0f416
Rituparna Ghosh <rituparna.ghosh@hih7.com>
2023-05-12 12:38:40 +0530

    added cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
+const cors = require("cors");
 
 const errorMiddleware = require("./middleware/error");
 
@@ -14,6 +15,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 //route imports
 const product = require("./routes/productRoute");
